7b52052f52f6d7e578c5678e2c9fcc23dde4de11
Darrell <darrelldjones13@gmail.com>
2014-05-10 17:58:53 -0500

    3/4 Challenge Reflection

--- week_3/4_manipulating_js_objects_solo_challenge/my_solution.js
+++ week_3/4_manipulating_js_objects_solo_challenge/my_solution.js
@@ -16,47 +16,74 @@ var terah = {
 1. Define a variable adam and use object literal notation to assign this variable 
    the value of a JavaScript Object object with no properties.
 
+// SET variable adam {}
+
 2. Give adam a name property with the value "Adam".
+
+// Assign "Adam" to adam.name
 
 3. Add a spouse property to terah and assign it the value of adam.
 
+// SET terah.spouse to adam
+
 4. Change the value of the terah weight property to 125.
+
+// SET terah.weight to 125
 
 5. Remove the eyeColor property from terah.
 
+// DELETE terah.eyeColor
+
 6. Add a spouse property to adam and assign it the value of terah.
+
+// SET adam.spouse to terah
 
 7. Add a children property to terah and and use object literal notation to assign 
    this variable the value of a JavaScript Object object with no properties
 
+// SET terah.children to {}
+
 8. Add a carson property to the value of the terah children property and assign it 
   the value of an object with the property name with a value of "Carson".
+
+// SET terah.children.carson to new object "Carson"
 
 9. Add a carter property to the value of the terah children property and assign it 
    the value of an object with the property name with a value of "Carter".
 
+// SET terah.children.carter to new object "Carter"
+
 10. Add a colton property to the value of the terah children property and assign it 
     the value of an object with the property name with a value of "Colton".
 
+// SET terah.children.colton to new object "Colton"
+
 11. Add a children property to adam and assign it the value of terah children.
 
-
+// SET adam.children equal to terah.children
 */
 
 // __________________________________________
 // Write your code below.
 
-
-
-
-
+var adam = {};
+adam.name = "Adam";
+terah.spouse = adam;
+terah.weight = 125;
+delete terah.eyeColor;
+adam.spouse = terah;
+terah.children = {};
+terah.children.carson = {name :"Carson"};
+terah.children.carter = {name :"Carter"};
+terah.children.colton = {name :"Colton"};
+adam.children = terah.children;
 
 
 
 // __________________________________________
 // Reflection: Use the reflection guidelines
 // 
-// 
+// After completing Challenge #3, this task was much easier to understand in terms of driver test code.  I did struggle a bit with where to place the terah.spouse in the code.  I first thought it needed to go within the properties of terah.  I quickly discovered that was incorrect.  I had to play around with some trial and error before discovering that it needed its own line. Overall, I feel more comfortable with the creation and editing of JS properties. 
 // 
 // 
 // 
